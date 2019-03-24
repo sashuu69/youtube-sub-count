80d96ade5ccc414470102a5be96e5d4fa3c71b01
Sashwat K <sashwat0001@gmail.com>
2019-03-24 13:00:32 +0530

    added required libraries

---
+++ youtube-sub-count/youtube-sub-count.ino
@@ -0,0 +1,16 @@
+#include <YoutubeApi.h> // Library to get data from youtube
+#include <ESP8266WiFi.h> // Library for Wifi communication
+#include <WiFiClientSecure.h> // Library for Server - Client communication
+#include <ArduinoJson.h> // Library to work on JSON input from youtube
+#include <Wire.h> // Library for OLED display
+#include <SSD1306.h> // Library for OLED display
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
