a7097108ecb8dce222a81001b9b0bbd1f835bae1
Sashwat K <sashwat0001@gmail.com>
2019-03-24 13:33:22 +0530

    working on  void setup

--- youtube-sub-count/youtube-sub-count.ino
+++ youtube-sub-count/youtube-sub-count.ino
@@ -5,9 +5,36 @@
 #include <Wire.h> // Library for OLED display
 #include <SSD1306.h> // Library for OLED display
 
-void setup() {
-  // put your setup code here, to run once:
+#define API_KEY "" // Paste your youtube API inside the quotes
+#define CHANNEL_ID "" // Paste your channel's ID inside the quotes
 
+char ssid[] = ""; // Enter your WiFi's SSID
+char password[] = ""; // ENter your WiFi's password
+
+SSD1306  display(0x3c, 4,5); // For OLED display
+WiFiClientSecure client; // For Client Server communication
+YoutubeApi api(API_KEY, client); // To get data from youtube
+
+unsigned long api_mtbs = 1000; //Increase the number to increase the API refresh rate. At present set to every 1 sec.
+unsigned long api_lasttime;
+int subs_count; // To store subscriber count
+int view_count; // To store view count
+
+void setup() {
+  Serial.begin(115200); // For serial print
+  display.init(); // For OLED display
+  display.flipScreenVertically(); // For OLED display
+  display.setFont(ArialMT_Plain_24); // For OLED display
+
+  //For establishing WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  //For OLED boot screen
+  display.drawString(0,6, "ZenoModiff"); // Replace text with your name
+  display.display();
+  delay(5000);
 }
 
 void loop() {
